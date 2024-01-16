85d2d7f8fe1030e4d37a1f88d8ed904a03c3230d
anka <anka.zatcepina@gmail.com>
2024-01-16 14:25:15 +0300

    API prompt text added

--- api/main.py
+++ api/main.py
@@ -1,11 +1,71 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+import logging
+from dotenv import load_dotenv
 
+from flask import Flask, jsonify, render_template, request
+
+from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import GigaChat
+from langchain.chains import LLMChain
+from langchain.chains.summarize import load_summarize_chain
+from langchain.document_loaders import WebBaseLoader
+from langchain.schema.messages import HumanMessage, SystemMessage
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+import json
+import re
+
+app = Flask(__name__)
+load_dotenv()
+CREDENTIALS = os.getenv('GIGACHAT_CREDENTIALS')
+giga = GigaChat(credentials=CREDENTIALS, verify_ssl_certs=False)
+logger = logging.getLogger(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route("/promt_text", methods=['POST'])
+def promt_text():
+    data = request.get_json()
+    if data is None:
+        return jsonify({ 'error': 'Missing input' }), 400
+    
+    map_prompt = data.get('map_prompt', 0)
+    combine_prompt = data.get('combine_prompt', 0)
+    text = data.get('text', 0)
+
+    num_tokens = giga.get_num_tokens(text)
+    logger.info(f'Num tokens: {num_tokens}')
+    text = re.sub(' +', ' ', text)
+    text = text.replace("\n", "")
+    #text = text.replace("�", "")
+    text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n", "."], chunk_size=10000, chunk_overlap=500)
+    splitted_text = text_splitter.create_documents([text])
+    logger.info(f'Num texts: {len(splitted_text)}')
+
+
+    map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text"])
+    combine_prompt_template = PromptTemplate(template=combine_prompt, input_variables=["text"])
+    summary_chain = load_summarize_chain(llm=giga,
+                                     chain_type='map_reduce',
+                                     map_prompt=map_prompt_template,
+                                     combine_prompt=combine_prompt_template,
+#                                      verbose=True
+                                    )
+    result = summary_chain.run(splitted_text)
+
+    response = {
+        "promted_text": result,
+    }
+    
+    logger.info(f'RESPONSE: {response}')
+    return json.dumps(response, ensure_ascii=True)
+    return jsonify({
+        "promted_text": result,
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
--- app/text_analizer.py
+++ app/text_analizer.py
@@ -13,6 +13,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import nest_asyncio
 
 import re
+import requests
 
 logger = logging.getLogger(__name__)
 
@@ -33,7 +34,7 @@ def get_file(user_id: str)-> str | None:
 
 def get_summary(user_id: str)->str:
     text = get_file(user_id)
-    #logger.info(text)
+    """#logger.info(text)
     num_tokens = giga.get_num_tokens(text)
     logger.info(f'Num tokens: {num_tokens}')
     text = re.sub(' +', ' ', text)
@@ -51,7 +52,7 @@ def get_summary(user_id: str)->str:
 #                                      verbose=True # Set verbose=True if you want to see the prompts being used
                                     )
     #output = summary_chain.run(splitted_text)
-
+"""
     if text == None:
         return "Извините, не получается проанализировать текст"  
     
@@ -62,7 +63,7 @@ def get_summary(user_id: str)->str:
         Краткое изложение текста должно быть не длинее 1 страницы A4 и на русском языке.
         ```{text}```
         """
-    map_prompt_template = PromptTemplate(template=prompt_template, input_variables=["text"])
+    """map_prompt_template = PromptTemplate(template=prompt_template, input_variables=["text"])
     combine_prompt_template = PromptTemplate(template=prompt_template, input_variables=["text"])
     summary_chain = load_summarize_chain(llm=giga,
                                      chain_type='map_reduce',
@@ -70,8 +71,26 @@ def get_summary(user_id: str)->str:
                                      combine_prompt=combine_prompt_template,
 #                                      verbose=True
                                     )
-    response = summary_chain.run(splitted_text)
-    return response
+    response = summary_chain.run(splitted_text)"""
+    
+    json = { 'map_prompt' :  prompt_template, 'combine_prompt':  prompt_template, 'text': text}
+    #response = web.post('/promt_text', json=json)
+    #response = requests.post('http://localhost/promt_text', json=json)
+    response = requests.post('http://192.168.50.8:8077/promt_text', json=json)
+    #response.text.decode('utf-8')
+    logger.info(f'STATUS {response.status_code}')
+    content_type = {response.headers['Content-Type']}
+    logger.info(f'CONTENT TYPE {content_type}')    
+    logger.info(f'CONTENT1 {response.content}')    
+    logger.info(f'CONTENT2 {response.content.decode("utf-8")}')    
+    logger.info(f'TEXT {response.text}')  
+    #logger.info(f'JSON {response.json}')
+    #if content_type == {'application/json'}:
+    if content_type == {'text/html; charset=utf-8'}:
+        #logger.info(f'HEADERS {rv.headers['Content-Type']}')
+        #result = response.json
+        return response.content.decode("utf-8")
+    return "Ошибка в API" 
 
     chat_template = ChatPromptTemplate.from_messages(
         [
